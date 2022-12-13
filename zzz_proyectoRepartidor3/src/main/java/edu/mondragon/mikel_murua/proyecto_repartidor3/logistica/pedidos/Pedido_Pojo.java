409f20b20280f6fb148b4f1b816cd86ac325dce7
MeikiO <mikel.murua@susosise.es>
2022-12-13 10:34:51 +0100

    Base estable para empezar con Admin ( Errores miscelaneos arreglados)

--- zzz_proyectoRepartidor3/src/main/java/edu/mondragon/mikel_murua/proyecto_repartidor3/logistica/pedidos/Pedido_Pojo.java
+++ zzz_proyectoRepartidor3/src/main/java/edu/mondragon/mikel_murua/proyecto_repartidor3/logistica/pedidos/Pedido_Pojo.java
@@ -113,7 +113,7 @@ public class Pedido_Pojo {
    
     @OneToMany(cascade=CascadeType.ALL,fetch = FetchType.EAGER)
     @JoinColumn(name="lineapedido_id")
-    //@org.hibernate.annotations.ForeignKey(name = "none")
+    @org.hibernate.annotations.ForeignKey(name = "none")
   	//desabilitamos la comprobacion del foreign key de pedidos por que sino nos dara error
   	// link-> https://stackoverflow.com/questions/41729709/how-do-i-disable-hibernate-foreign-key-constraint-on-a-bidirectional-association
   	
